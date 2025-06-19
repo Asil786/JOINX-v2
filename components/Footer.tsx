b6ad8551c3959f0e3c25b8f6e757f08056a6d284
Asil786 <asilshaikh123@gmail.com>
2025-06-19 18:11:36 +0530

    init

---
+++ components/Footer.tsx
@@ -0,0 +1,8 @@
+const Footer = () => (
+  <footer className="w-full py-4 text-center text-gray-500">
+    <div>© {new Date().getFullYear()} JOINX. All rights reserved.</div>
+    <div>Made with ❤️ by Mohd Asil.</div>
+  </footer>
+);
+
+export default Footer;
